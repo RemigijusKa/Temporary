40c384641b1f2daa47343ec8d3280a7a289e1c5a
RemigijusKa <38855837+RemigijusKa@users.noreply.github.com>
2018-05-14 14:50:10 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static final String TYPE_EU ="EU";
+
+    public static void main( String[] args ) {
+        String currencyCode = "cny";
+        String dateFrom = "2018-05-01";
+        String dateTo = "2018-05-04";
+
+        RequestService requestService = new RequestService();
+        try {
+            requestService.sendRequest(TYPE_EU, currencyCode, dateFrom, dateTo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
