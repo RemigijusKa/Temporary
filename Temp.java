32003af96895ff3372ee63ef1df170d1be4649a7
RemigijusKa <38855837+RemigijusKa@users.noreply.github.com>
2018-05-14 14:49:15 +0300

    Update Temp.java

--- Temp.java
+++ Temp.java
@@ -1,3 +1,16 @@
+import jdk.internal.org.xml.sax.InputSource;
+import jdk.internal.org.xml.sax.XMLReader;
+import org.w3c.dom.*;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+
 public class RequestService {
 
     private final String SERVER = "http://old.lb.lt/webservices/fxrates/FxRates.asmx/getFxRatesForCurrency?";
@@ -5,13 +18,17 @@ public class RequestService {
     public void sendRequest(String type, String currencyCode, String dateFrom, String dateTo) throws Exception {
         String url = SERVER + "tp=" + type + "&ccy=" + currencyCode + "&dtFrom=" + dateFrom + "&dtTo=" + dateTo;
 
-        URL xmlUrl = new URL(url);
-        InputStream is = xmlUrl.openStream();
-        Document doc = parse(is);
+        String urlTwo = "http://old.lb.lt/webservices/fxrates/FxRates.asmx/getFxRatesForCurrency?tp=EU&ccy=CNY&dtFrom=2018-05-01&dtTo=2018-05-08";
+        URLConnection con = new URL(urlTwo).openConnection();
+        con.addRequestProperty("Accept", "application/xml");
+        InputStream is = con.getInputStream();
+        Document xml = parse(is);
+        System.out.println(xml);
 
-        }
 
-    public static Document parse (InputStream is) {
+    }
+
+    public static Document parse(InputStream is) {
         Document ret = null;
         DocumentBuilderFactory domFactory;
         DocumentBuilder builder;
@@ -23,8 +40,7 @@ public class RequestService {
             builder = domFactory.newDocumentBuilder();
 
             ret = builder.parse(is);
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             System.out.println("unable to load XML: " + ex);
         }
         return ret;
